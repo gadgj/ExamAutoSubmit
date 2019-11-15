0442e22267a3ff6c4b067ea7f13430aa7fcfa9d5
gadgj <ziqingzhao20@gmail.com>
2019-11-15 14:55:21 +0800

    第一版答题

---
+++ ConsoleApplicationExamAutoSubmit/ExamModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationExamAutoSubmit
+{
+    class ExamModel
+    {
+        public string EID { get; set; }
+        public string ECONTENT { get; set; }
+        public string ETYPE { get; set; }
+        public string EA { get; set; }
+        public string EB { get; set; }
+        public string EC { get; set; }
+        public string ED { get; set; }
+        public string EANSWERX { get; set; }
+        public string EANSWERP { get; set; }
+        public string EXAMTYPENO { get; set; }
+        public string EXAMNOTE { get; set; }
+        public string CREATEMAN { get; set; }
+        public string CREATETIME { get; set; }
+        public string UPDATEMAN { get; set; }
+        public string UPDATETIME { get; set; }
+    }
+}
--- ConsoleApplicationExamAutoSubmit/Program.cs
+++ ConsoleApplicationExamAutoSubmit/Program.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace ConsoleApplicationExamAutoSubmit
 {
     class Program
     {
+        private static string urlGet = "http://wechat.sctcn.com/SctWeChat_Exam/WX/GetExamList?ExamTypeNo&Pname=赵子清&Pno=SCT01975&Pdepart=信息技术部&Type=WX";
+        private static string urlPost = "http://wechat.sctcn.com/SctWeChat_Exam/WX/SubmitExam";
         static void Main(string[] args)
         {
+            string resGet = HttpHelper.Get(urlGet);
+            Console.WriteLine(resGet);
+            List<ExamModel> objResGet = JsonConvert.DeserializeObject<List<ExamModel>>(resGet);
 
+            foreach(ExamModel objEle in objResGet)
+            {
+                objEle.EXAMNOTE = string.IsNullOrEmpty(objEle.EANSWERX) ? objEle.EANSWERP : objEle.EANSWERX;                
+            }
+
+            string resAnswerGet = JsonConvert.SerializeObject(objResGet);
+
+            string resPost  = HttpHelper.Post(urlPost, resAnswerGet, "application/json", Encoding.UTF8);
+
+            Console.WriteLine("分数：" + resPost);
+
+            Console.ReadKey();
         }
     }
 }
