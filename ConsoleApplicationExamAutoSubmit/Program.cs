e1fba053a1609ef592a382cf493bc341cedba8a0
gadgj <ziqingzhao20@gmail.com>
2019-11-18 14:16:11 +0800

    错误退出

--- ConsoleApplicationExamAutoSubmit/Program.cs
+++ ConsoleApplicationExamAutoSubmit/Program.cs
@@ -9,17 +9,24 @@ namespace ConsoleApplicationExamAutoSubmit
 {
     class Program
     {
-        private static string urlGet = "http://wechat.sctcn.com/SctWeChat_Exam/WX/GetExamList?ExamTypeNo&Pname=赵子清&Pno=SCT01975&Pdepart=信息技术部&Type=WX";
+        private static string urlGet = "http://wechat.sctcn.com/SctWeChat_Exam/WX/GetExamList?ExamTypeNo&Pname=赵子清&Pno=SCT01976&Pdepart=信息技术部&Type=WX";
         private static string urlPost = "http://wechat.sctcn.com/SctWeChat_Exam/WX/SubmitExam";
         static void Main(string[] args)
         {
             string resGet = HttpHelper.Get(urlGet);
             Console.WriteLine(resGet);
+            if(!resGet.StartsWith("["))
+            {
+                Console.WriteLine("试题获取错误！");
+                Console.ReadKey();
+                return;
+            }
+
             List<ExamModel> objResGet = JsonConvert.DeserializeObject<List<ExamModel>>(resGet);
 
             foreach(ExamModel objEle in objResGet)
             {
-                objEle.EXAMNOTE = string.IsNullOrEmpty(objEle.EANSWERX) ? objEle.EANSWERP : objEle.EANSWERX;                
+                objEle.EXAMNOTE = string.IsNullOrEmpty(objEle.EANSWERX) ? objEle.EANSWERP : objEle.EANSWERX;
             }
 
             string resAnswerGet = JsonConvert.SerializeObject(objResGet);
